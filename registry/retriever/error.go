55b297649b94cc9b4bb226e2550c50a1e6710973
Guilherme Ferreira <guilherme.ferreira@protofire.io>
2024-10-11 08:45:40 -0300

    centrifuge go substrate rpc client without signature module

---
+++ registry/retriever/error.go
@@ -0,0 +1,14 @@
+package retriever
+
+import libErr "github.com/didaunesp/no-signature-go-substrate-rpc-client-v4/error"
+
+const (
+	ErrInternalStateUpdate      = libErr.Error("internal state update")
+	ErrBlockRetrieval           = libErr.Error("block retrieval")
+	ErrExtrinsicDecoding        = libErr.Error("extrinsic parsing")
+	ErrMetadataRetrieval        = libErr.Error("metadata retrieval")
+	ErrExtrinsicDecoderCreation = libErr.Error("extrinsic decoder creation")
+	ErrStorageEventRetrieval    = libErr.Error("storage event retrieval")
+	ErrEventParsing             = libErr.Error("event parsing")
+	ErrEventRegistryCreation    = libErr.Error("event registry creation")
+)
